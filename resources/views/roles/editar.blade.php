be3bbfbaf82b8fbaeb471795b7065f39538f3f8a
Mark <marks88martinez@gmail.com>
2023-06-30 15:10:58 -0400

    roles template

--- resources/views/roles/editar.blade.php
+++ resources/views/roles/editar.blade.php
@@ -1,57 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="section">
-        <div class="section-header">
-            <h3 class="page__heading">Editar Rol</h3>
-        </div>
-        <div class="section-body">
+
+        <!-- ///////////////////////////////////// -->
+        <div class="container-fluid">
+
+            <!-- start page title -->
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            
-                        @if ($errors->any())                                                
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
+                                <li class="breadcrumb-item active">Form Elements</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">Crear Usuarios</h4>
+                        @if ($errors->any())
                             <div class="alert alert-dark alert-dismissible fade show" role="alert">
-                            <strong>¡Revise los campos!</strong>                        
-                                @foreach ($errors->all() as $error)                                    
+                            <strong>¡Revise los campos!</strong>
+                                @foreach ($errors->all() as $error)
                                     <span class="badge badge-danger">{{ $error }}</span>
-                                @endforeach                        
+                                @endforeach
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                             </div>
                         @endif
-
-                    {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <label for="">Nombre del Rol:</label>      
-                                {!! Form::text('name', null, array('class' => 'form-control')) !!}
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <label for="">Permisos para este Rol:</label>
-                                <br/>
-                                @foreach($permission as $value)
-                                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-                                    {{ $value->name }}</label>
-                                <br/>
-                                @endforeach
-                            </div>
-                        </div>
-                        
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                        
-                    </div>
-                    {!! Form::close() !!}
-
-                        </div>
                     </div>
                 </div>
             </div>
+            <!-- end page title -->
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                        <div class="tab-pane" id="input-types-preview" role="tabpanel">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                        {{-- ///////////////////// --}}
+
+                                        {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
+                                            <div class="row">
+
+                                                <div class="mb-3">
+                                                    <label for="">Nombre del Rol:</label>
+                                                    {!! Form::text('name', null, array('class' => 'form-control')) !!}
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="">Permisos para este Rol:</label>
+                                                    <br/>
+                                                    @foreach($permission as $value)
+                                                        <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
+                                                        {{ $value->name }}</label>
+                                                        <br/>
+                                                    @endforeach
+
+                                                </div>
+
+                                            </div>
+                                            <div class="mb-3">
+                                                <button type="submit" class="btn btn-primary">Guardar</button>
+                                            </div>
+                                        {!! Form::close() !!}
+
+                                        {{-- ///////////////////// --}}
+                                        </div> <!-- end col -->
+
+
+                                    </div>
+                                    <!-- end row-->
+                                </div> <!-- end preview-->
+
+
+
+                        </div> <!-- end card-body -->
+                    </div> <!-- end card -->
+                </div><!-- end col -->
+            </div><!-- end row -->
         </div>
-    </section>
+        <!-- ///////////////////////////////////// -->
+
 @endsection
